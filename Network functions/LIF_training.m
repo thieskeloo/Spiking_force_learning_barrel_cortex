4e78fd4fa31a34fbc741dccb84eb24f7a464660a
Thijs van Loo <thijsvanloo4@gmail.com>
2023-01-20 13:06:28 +0100

    bug fix in training output
    fixed error from training output saving, where wrong variable name was used, also added weight change per training trial

--- Network functions/LIF_training.m
+++ Network functions/LIF_training.m
@@ -1,7 +1,7 @@
 function [training_output] = LIF_training(param, scale_param, savefolder)
-% LIF_TRAINING prepares the weights and input of the network,
-% and runs the test and train trials of the network with spiking neurons,
-% based on the leaky integrate and firing model
+% LIF_TRAINING prepares the weights and input of the network, and runs the
+% test and train trials of the network with spiking neurons, based on the
+% leaky integrate and firing model
 % Input:
 %   * param = input parameters
 %   * scale_param = weight scaling parameters
@@ -43,7 +43,7 @@ input = Win.*rand(RandStream('mt19937ar','Seed',seed),N,N_th).*(rand(RandStream(
 % static weights
 if Pexc == 0
     static = G*(randn(RandStream('mt19937ar','Seed',seed),N,N)).*(rand(RandStream('mt19937ar','Seed',seed),N,N)<p)/(sqrt(N)*p);
-    
+
     % set the row average to be zero, explicitly, to induce chaotic spiking
     for i = 1:1:N
         QS = find(abs(static(i,:))>0);
@@ -52,23 +52,23 @@ if Pexc == 0
 else
     % apply Dale's law
     disp('Dales law is applied')
-    
+
     % number of excitatory and inhibitory neurons
     Nexc = round(Pexc * N);
     Nini = N - Nexc;
-    
+
     % initiate both halves of the static weights
     static_exc = G*abs(randn(N, Nexc)).*(rand(N, Nexc) < p)/(sqrt(N)*p);
     static_ini = G*abs(randn(N, Nini)).*-(rand(N, Nini) < p)/(sqrt(N)*p);
-    
+
     % set the mean input of each neuron to zero by adjusting the inibition
     for i = 1:N
         QS = find(static_ini(i, :) ~= 0);
-        
+
         row_sum = sum(static_exc(i, :)) + sum(static_ini(i, :));
         static_ini(i, QS) = static_ini(i, QS) - row_sum/length(QS);
     end
-    
+
     static = [static_exc static_ini];
 end
 
@@ -92,21 +92,21 @@ test_trials = param.test_trials;
 if param.makespikes || strcmp('ConvTrace', input_type) || strcmp('PSTH', input_type)
     % load the KernelStruct
     filename = ['.' f 'Input' f 'KernelStruct.mat'];
-    
+
     if ~exist(filename, 'file')
         error('KernelStruct.mat is not in the input folder')
     end
-    
+
     KernelStruct = load(filename);
     KernelStruct = KernelStruct.KernelStruct;
-    
+
     % load the whiskmat
     filename = ['.' f 'Input' f 'whiskmat.mat'];
-    
+
     if ~exist(filename, 'file')
         error('whiskmat.mat is not in the input folder')
     end
-    
+
     whiskmat = load(filename);
     whiskmat = whiskmat.filtered_whiskmat;
 end
@@ -114,20 +114,20 @@ end
 % run for N amount of epochs
 for epoch = 1:N_total
     disp(['Epoch nr. ', num2str(epoch)])
-    
+
     % ----------------------------TRAINING---------------------------------
     disp(['Training network, number of trials = ', num2str(N_train)])
-    
+
     % apply FORCE learning
     FORCE = param.FORCE;
     weight_change = zeros(N_train, 1);
-    
+
     for trial = 1:N_train
-        
+
         % NETWORK INPUT
         trialId = train_trials(trial).trial;
         if param.makespikes || strcmp('ConvTrace', input_type) || strcmp('PSTH', input_type)
-            
+
             % get the trial session and create the spikingstruct
             session = train_trials(trial).session;
             SpikeTrainStruct = make_trial_spikes(session, trialId,...
@@ -138,16 +138,16 @@ for epoch = 1:N_total
             file = load( ['./Spiking structures/', trial_mat]);
             SpikeTrainStruct = file.SpikeTrainStruct;
         end
-        
+
         % get the pole location and the input struct and target function
         pole = train_trials(trial).ytrain;
         [thalamus_input, target] =...
             reservoir_input(SpikeTrainStruct, input, N, N_th, pole, rate, input_type);
-        
-        % SIMULATE NETWORK
+
+        % SIMULATE NETWORK 
         % save the old output weights
         old_output = output;
-        
+
         if strcmp('spikes', input_type)
             [err, output, Zx, Z_out, tspikes] =...
                 LIF_spiking_network(param, weights, thalamus_input, target, FORCE);
@@ -155,21 +155,21 @@ for epoch = 1:N_total
             [err, output, Zx, Z_out, tspikes] =...
                 LIF_spiking_network_no_filt(param, weights, thalamus_input, target, FORCE);
         end
-        
+
         % calculate the weight difference and update the output weights
         d_output = old_output - output;
         weight_change(trial, 1) = sum(abs(d_output));
         weights.output = output;
-        
+
         % save the validation trials and first touches
         train_data.trials{trial} = trialId;
         train_data.first_touches(trial,1) = test_trials(trial).first_touch;
-        
+
         % get the spikinging statistics
         trial_length = length(Z_out);
         [A_t, ISI, Cv] = spike_stats(tspikes, trial_length , N);
-        
-        weights.output = output_weights;
+
+        train_data.output_weights{trial} = output;
         train_data.error{trial} = err;
         train_data.Zx{trial} = Zx;
         train_data.Z_out{trial} = Z_out;
@@ -180,18 +180,18 @@ for epoch = 1:N_total
     end
 
 
-    
+
     % --------------------------VALIDATION---------------------------------
     disp(['Testing network, number of trials = ', num2str(N_test)])
-    
+
     % validation trials
     FORCE = false;
     for trial = 1:N_test
-        
+
         % NETWORK INPUT
         trialId = test_trials(trial).trial;
         if param.makespikes || strcmp('ConvTrace', input_type) || strcmp('PSTH', input_type)
-            
+
             % get the trial session and create the spikingstruct
             session = test_trials(trial).session;
             SpikeTrainStruct = make_trial_spikes(session, trialId,...
@@ -201,16 +201,16 @@ for epoch = 1:N_total
             trial_mat = test_trials(trial).spike_struct;
             load(['./Spiking structures/', trial_mat]);
         end
-        
+
         % save the validation trials and first touches
         test_output.trials{trial} = trialId;
         test_output.first_touches(trial,1) = test_trials(trial).first_touch;
-        
+
         % get the pole location and the input struct and target function
         pole = test_trials(trial).ytrain;
         [thalamus_input, target] =...
             reservoir_input(SpikeTrainStruct, input, N, N_th, pole, rate, input_type);
-        
+
         % SIMULATE NETWORK
         if strcmp('spikes', input_type)
             [err, output_weights, Zx, Z_out, tspikes] =...
@@ -219,15 +219,15 @@ for epoch = 1:N_total
             [err, output_weights, Zx, Z_out, tspikes] =...
                 LIF_spiking_network_no_filt(param, weights, thalamus_input, target, FORCE);
         end
-        
+
         %TODO
         % input_save{trial}.neuron_input = thalamus_input;
-        
-        
+
+
         % get the spikinging statistics
         trial_length = length(Z_out);
         [A_t, ISI, Cv] = spike_stats(tspikes, trial_length , N);
-        
+
         weights.output = output_weights;
         test_output.error{trial} = err;
         test_output.Zx{trial} = Zx;
@@ -237,11 +237,11 @@ for epoch = 1:N_total
         test_output.stats{trial}.ISI = ISI;
         test_output.stats{trial}.Cv = Cv;
     end
-    
+
     % calculate the test accuracy
     acc = val_acc(N_test, test_output.Z_out, test_output.Zx, test_output.first_touches);
     disp(['Test accuracy = ', num2str(acc)])
-    
+
     % save the training data per epoch
     training_output(epoch).weight_change = weight_change;
     training_output(epoch).train_trials = train_trials;
